3a85b664dd58b3adadd9c668e5bb087d3cf99f2a
pascal.desprez <pascal.desprez@fr.ibm.com>
2021-12-22 23:37:52 +0100

    initial commit

---
+++ src/main/java/springbatch/toolbox/app/cli/command/AbstractSubCommand.java
@@ -0,0 +1,64 @@
+package springbatch.toolbox.app.cli.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * This class provides some basic functionality for commands.
+ */
+abstract class AbstractSubCommand {
+
+	@Autowired
+	protected JobRepository jobRepository;
+
+	@Autowired
+	private JobExplorer jobExplorer;
+
+	/**
+	 * Find all executions for the job identified by jobIdentifier.
+	 *
+	 * @param jobIdentifier The name of the job
+	 * @return Returns a list of all executions for the given job.
+	 * @throws NoSuchJobException If the job does not exists.
+	 */
+	protected List<JobExecution> findAllExecutions(final String jobIdentifier) throws NoSuchJobException {
+
+		final List<JobExecution> jobExecutions = new ArrayList<>();
+		for (final JobInstance jobInstance : jobExplorer.findJobInstancesByJobName(jobIdentifier, 0,
+				jobExplorer.getJobInstanceCount(jobIdentifier))) {
+			jobExecutions.addAll(jobExplorer.getJobExecutions(jobInstance));
+		}
+		return jobExecutions;
+	}
+
+	/**
+	 * Find all executions for the job identified by jobIdentifier which match the
+	 * given status.
+	 *
+	 * @param jobIdentifier The name of the job
+	 * @param batchStatus   The status to look for
+	 * @return Returns a list of all executions for the given job with the given
+	 *         status.
+	 * @throws NoSuchJobException If the job does not exists.
+	 */
+	protected List<JobExecution> findJobExecutionsWithStatus(final String jobIdentifier, final BatchStatus batchStatus)
+			throws NoSuchJobException {
+
+		final List<JobExecution> result = new ArrayList<>();
+		for (final JobExecution jobExecution : findAllExecutions(jobIdentifier)) {
+			if (jobExecution.getStatus() != batchStatus) {
+				result.add(jobExecution);
+			}
+		}
+		return result.isEmpty() ? null : result;
+	}
+
+}
