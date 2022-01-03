b5fcd9d38bc1778f12d38dd7da1daab0e80f6363
pascal.desprez <pascal.desprez@fr.ibm.com>
2022-01-03 23:28:49 +0100

    refactor and polish

--- src/test/java/springbatch/toolbox/app/cli/command/AbandonCommandTest.java
+++ src/test/java/springbatch/toolbox/app/cli/command/AbandonCommandTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
@@ -19,6 +18,7 @@ import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 import picocli.CommandLine;
 import springbatch.toolbox.app.cli.SpringBatchCli;
@@ -26,6 +26,7 @@ import springbatch.toolbox.app.cli.SpringBatchCli;
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = NONE, classes = SpringBatchCli.class)
 @TestInstance(Lifecycle.PER_CLASS)
+@Sql({ "/abandon-h2.sql" })
 class AbandonCommandTest {
 
 	@Autowired
@@ -47,8 +48,6 @@ class AbandonCommandTest {
 	void abandon_job_command_should_success()
 			throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
 		// Given
-		final Long jobExecutionId = fixtureWithJobExecution();
-
 		final CommandLine cmd = new CommandLine(abandonCommand, factory);
 
 		// When
@@ -56,17 +55,9 @@ class AbandonCommandTest {
 
 		// Then
 		assertThat(exitCode).isEqualTo(0);
-		final JobExecution found = jobExplorer.getJobExecution(jobExecutionId);
+		final JobExecution found = jobExplorer.getJobExecution(-1L);
 		assertThat(found.getStatus()).isEqualTo(BatchStatus.ABANDONED);
 
-	}
-
-	private Long fixtureWithJobExecution()
-			throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
-		final JobExecution newJobExecution = jobRepository.createJobExecution("jobTest", new JobParameters());
-		newJobExecution.setStatus(BatchStatus.STARTED);
-		jobRepository.update(newJobExecution);
-		return newJobExecution.getId();
 	}
 
 	@AfterAll
